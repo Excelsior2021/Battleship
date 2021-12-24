057204b9279af1f0899476682062ff5a3093ec95
Jonathan Kila <temidee11@yahoo.co.uk>
2021-12-24 16:42:02 +0000

    24/12/2021 - ships on board

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
 const coordinates = document.getElementsByClassName("coordinate");
+
 for (let i = 0; i < coordinates.length; i++) {
     coordinates[i].onclick = log;
 }
 
-
 function log(evt) {
     let id = evt.target.id;
     let coordinate = document.getElementById(id)
+    let player_coordinate = document.getElementById("coordinate");
 
-    console.log(id);
-    coordinate.classList.add("hit")
+    if(confirm(id)) {
+        coordinate.classList.add("hit")
+    }
+    player_coordinate.innerHTML = id;
+
 }
+
+function confirm(id) {
+    for(let i = 0; i<ships.length; i++) {
+        for(let j = 0; j<ships[i].location.length; j++) {
+            if(id === ships[i].location[j]) {
+                return true
+            }
+        }
+    } 
+    return false
+}
+
+ships = [
+    {name: "Carrier", size: 5, location: ["A1", "B1", "C1", "D1", "E1"],},
+    {name: "Battleship", size: 4, location: ["J7", "J8", "J9", "J10"],},
+    {name: "Cruiser", size: 3, location: ["D4", "E4", "F4"],},
+    {name: "Submarine", size: 3, location: ["E7", "E8", "E9"],},
+    {name: "Destroyer", size: 2, location: ["I2", "I3"],},
+]
