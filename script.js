7815e17f1a8c5c29c09ca15203c5cb482d590502
Jonathan Kila <temidee11@yahoo.co.uk>
2021-12-25 15:49:41 +0000

    25/12/21 - v1

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 const coordinates = document.getElementsByClassName("coordinate");
+let count = 0
+let alreadyShot = []
+const ships = [
+    {name: "Carrier", size: 5, location: ["A1", "B1", "C1", "D1", "E1"],},
+    {name: "Battleship", size: 4, location: ["J7", "J8", "J9", "J10"],},
+    {name: "Cruiser", size: 3, location: ["D4", "E4", "F4"],},
+    {name: "Submarine", size: 3, location: ["E7", "E8", "E9"],},
+    {name: "Destroyer", size: 2, location: ["I2", "I3"],},
+]
 
 for (let i = 0; i < coordinates.length; i++) {
     coordinates[i].onclick = log;
@@ -8,13 +17,27 @@ function log(evt) {
     let id = evt.target.id;
     let coordinate = document.getElementById(id)
     let player_coordinate = document.getElementById("coordinate");
+    let confirmation = document.getElementById("confirmation");
+    let shots = document.getElementById("shots");
 
-    if(confirm(id)) {
-        coordinate.classList.add("hit")
+    if(alreadyShotFunction(id, alreadyShot)) {
+        confirmation.innerHTML = "You have already shot this coordinate. Select another!";
+        player_coordinate.innerHTML = id;
+    } else {
+            if(confirm(id)) {
+                coordinate.classList.add("hit")
+                confirmation.innerHTML = "You hit a ship!"
+            } else {
+                coordinate.classList.add("miss")
+                confirmation.innerHTML = "Missed!"
+            }
+            player_coordinate.innerHTML = id;
+            count++;
+            shots.innerHTML = "Shots taken: " + count
+            alreadyShot.push(id)
     }
-    player_coordinate.innerHTML = id;
-
 }
+
 
 function confirm(id) {
     for(let i = 0; i<ships.length; i++) {
@@ -27,10 +50,11 @@ function confirm(id) {
     return false
 }
 
-ships = [
-    {name: "Carrier", size: 5, location: ["A1", "B1", "C1", "D1", "E1"],},
-    {name: "Battleship", size: 4, location: ["J7", "J8", "J9", "J10"],},
-    {name: "Cruiser", size: 3, location: ["D4", "E4", "F4"],},
-    {name: "Submarine", size: 3, location: ["E7", "E8", "E9"],},
-    {name: "Destroyer", size: 2, location: ["I2", "I3"],},
-]
+function alreadyShotFunction(id, alreadyShot) {
+    for(i=0;i<alreadyShot.length;i++) {
+        if(id===alreadyShot[i]) {
+            return true
+        }
+    }
+    return false
+}
